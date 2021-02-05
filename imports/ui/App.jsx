45de4bfa7cfe451ef8a7f3975fff02c0bcdb280e
Jim Neuendorf <jim.neuendorf@gmx.de>
2021-02-05 23:48:04 +0100

    added navigation

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,20 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { useTracker } from 'meteor/react-meteor-data'
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
+    Link,
     Redirect,
+    Route,
+    Switch,
 } from 'react-router-dom'
+import { Menu, Spin } from 'antd'
+import { BarsOutlined, HomeOutlined, SettingOutlined } from '@ant-design/icons'
+
 import 'antd/dist/antd.css'
 
+// import * as Breakpoints from './breakpoints'
 import * as Routes from './routes'
-import * as Pages from './pages/'
-import { Spin } from 'antd'
+import * as Pages from './pages'
 
 
 export const App = () => {
@@ -21,29 +25,50 @@ export const App = () => {
             user,
         }
     })
+    const [currentTab, setCurrentTab] = useState(
+        window.location.pathname.replace(/\//g, '')
+    )
     console.log('APP: user?', user, isLoading)
 
     if (isLoading) {
         return <Spin tip='Loading' />
     }
 
+
     return <Router>
         {
             user
-                ? <Switch>
-                    <Route path={Routes.OVERVIEW}>
-                        <Pages.Overview />
-                    </Route>
-                    <Route path={Routes.HISTORY}>
-                        <Pages.History />
-                    </Route>
-                    <Route path={Routes.SETTINGS}>
-                        <Pages.Settings />
-                    </Route>
-                    <Route>
-                        <Pages.NotFound />
-                    </Route>
-                </Switch>
+                ? <Fragment>
+                    <Menu
+                        mode="horizontal"
+                        selectedKeys={[currentTab]}
+                        onClick={({ key }) => setCurrentTab(key)}
+                    >
+                        <Menu.Item key="overview" icon={<HomeOutlined />}>
+                            <Link to={Routes.OVERVIEW}>Overview</Link>
+                        </Menu.Item>
+                        <Menu.Item key="history" icon={<BarsOutlined />}>
+                            <Link to={Routes.HISTORY}>History</Link>
+                        </Menu.Item>
+                        <Menu.Item key="settings" icon={<SettingOutlined />}>
+                            <Link to={Routes.SETTINGS}>Settings</Link>
+                        </Menu.Item>
+                    </Menu>
+                    <Switch>
+                        <Route path={Routes.OVERVIEW}>
+                            <Pages.Overview />
+                        </Route>
+                        <Route path={Routes.HISTORY}>
+                            <Pages.History />
+                        </Route>
+                        <Route path={Routes.SETTINGS}>
+                            <Pages.Settings />
+                        </Route>
+                        <Route>
+                            <Pages.NotFound />
+                        </Route>
+                    </Switch>
+                </Fragment>
                 : <Switch>
                     <Route path={Routes.LOGIN}>
                         <Pages.Login />
---
+++ imports/ui/breakpoints.jsx
@@ -0,0 +1,20 @@
+import { useMediaQuery } from 'react-responsive'
+
+
+const MOBILE_MAX = 768
+const TABLET_MAX = 992
+
+
+export const Desktop = ({ children }) => {
+    return useMediaQuery({ minWidth: TABLET_MAX }) && children
+}
+
+
+export const Tablet = ({ children }) => {
+    return useMediaQuery({ minWidth: MOBILE_MAX, maxWidth: TABLET_MAX - 1 }) && children
+}
+
+
+export const Mobile = ({ children }) => {
+    return useMediaQuery({ maxWidth: MOBILE_MAX - 1 }) && children
+}
