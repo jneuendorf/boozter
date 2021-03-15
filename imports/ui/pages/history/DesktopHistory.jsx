054b8b0382c9235076515bff7a88edfdd4952f58
Jim Neuendorf <jim.neuendorf@gmx.de>
2021-03-15 23:06:49 +0100

    desktop table like mobile

--- imports/ui/pages/history/DesktopHistory.jsx
+++ imports/ui/pages/history/DesktopHistory.jsx
@@ -1,19 +1,90 @@
-import React, { useCallback, useState } from 'react'
+import React, { Fragment, useCallback, useState } from 'react'
+import { Button } from 'antd'
 
-import { HistoryTable } from './HistoryTable'
+import { Wrapper } from '../../Wrapper'
+import {
+    HistoryTable,
+    ExpandableHistoryTable,
+} from './HistoryTable'
+import { CloseOutlined, DeleteOutlined } from '@ant-design/icons'
+
+
+const removeHistoryRecords = (ids) => {
+    Meteor.call('history.removeMany', ids)
+}
 
 
 export const DesktopHistory = ({ history }) => {
     const [isDeleteMode, setDeleteMode] = useState(false)
+    const [selectedRowIds, setSelectedRowIds] = useState([])
 
     const toggleDeleteMode = useCallback(
         () => setDeleteMode(!isDeleteMode),
         [setDeleteMode, isDeleteMode],
     )
+    const onRowSelectionChange = useCallback(
+        (selectedRowKeys, selectedRows) => {
+            setSelectedRowIds(
+                selectedRows.map(row => row.records)
+                    .flat()
+                    .map(record => record._id)
+            )
+        },
+        [setSelectedRowIds],
+    )
+    const removeSelectedRowIds = useCallback(
+        () => {
+            removeHistoryRecords(selectedRowIds)
+            setDeleteMode(false)
+        },
+        [selectedRowIds, setDeleteMode],
+    )
 
-    return <HistoryTable
-        dataSource={history}
-        expandRowByClick={false}
-        size='middle'
-    />
+    return <Fragment>
+        {
+            isDeleteMode
+                ? <HistoryTable
+                    dataSource={history}
+                    size='small'
+                    rowSelection={{
+                        type: 'checkbox',
+                        onChange: onRowSelectionChange,
+                    }}
+                />
+                : <ExpandableHistoryTable
+                    dataSource={history}
+                    expandRowByClick={true}
+                    size='small'
+                />
+        }
+        <Wrapper className='history--buttons'>
+            <Button
+                icon={
+                    isDeleteMode
+                        ? <CloseOutlined />
+                        : <DeleteOutlined />
+                }
+                inline={isDeleteMode}
+                className='toggle-delete-mode'
+                onClick={toggleDeleteMode}
+            >
+                {
+                    isDeleteMode
+                        ? 'cancel'
+                        : 'delete rows'
+                }
+            </Button>
+            {
+                isDeleteMode && <Button
+                    type='warning'
+                    inline
+                    icon={<DeleteOutlined />}
+                    disabled={selectedRowIds.length === 0}
+                    onClick={removeSelectedRowIds}
+                >
+                    delete selected
+                    </Button>
+            }
+        </Wrapper>
+    </Fragment>
 }
